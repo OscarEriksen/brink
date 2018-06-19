51d86654d5f34723ed20dfe2a5d6817920aafede
Peter Brinck <peter@peterbrinck.dk>
2018-06-19 19:50:52 +0200

    Better install command

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -3,29 +3,43 @@
 namespace PeterBrinck\Brink\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 use PeterBrinck\Brink\Brink;
 
 class InstallCommand extends Command
 {
 
-    protected $signature = 'brink';
+    protected $signature = 'brink {--force}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Install Brink';
+    protected $description = 'Installing Brink';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        Brink::install();
+        if ($this->option('force')) {
+            Brink::install();
 
+            $this->successfully();
+        } else {
+            $filesystem = new Filesystem;
+            if ($filesystem->exists(resource_path('assets/sass/app.scss'))) {
+                if ($this->confirm('A file called app.scss already exists. Are you sure you want to install Brink? This will overwrite the current files.')) {
+                    Brink::install();
+
+                    $this->successfully();
+                } else {
+                    $this->line('That\'s okay.');
+                    $this->line('Brink will not be installed. If this was a mistake, run the command again and be sure to accept when asked for confirmation or use the --force option.');
+                }
+            } else {
+                Brink::install();
+
+                $this->successfully();
+            }
+        }
+    }
+
+    public function successfully()
+    {
         $this->info('Brink installed successfully.');
         $this->info('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
     }
