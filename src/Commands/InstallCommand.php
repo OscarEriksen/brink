dd9994f757779715e1de510dddb700ce9c77fe6b
Oscar Eriksen <eriksenlundin@gmail.com>
2018-06-24 04:13:22 +0200

    Shortening the code down a bit
    Not 100% tested - sorry - I will test it later on (it's 04:13 AM here in Sweden), hope it's OK. Don't merge.

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace PeterBrinck\Brink\Commands;
-
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 use PeterBrinck\Brink\Brink;
@@ -9,34 +8,27 @@ use PeterBrinck\Brink\Brink;
 class InstallCommand extends Command
 {
     protected $signature = 'brink {--force}';
-
     protected $description = 'Installing Brink';
 
     public function handle()
     {
-        if ($this->option('force')) {
+        $filesystem = new Filesystem;
+
+        if ($this->option('force') || (!$filesystem->exists(resource_path('assets/sass/app.scss'))))
+        {
             Brink::install();
-
-            $this->successfully();
-        } else {
-            $filesystem = new Filesystem;
-            if ($filesystem->exists(resource_path('assets/sass/app.scss'))) {
-                if ($this->confirm('A file called app.scss already exists. Are you sure you want to install Brink? This will overwrite the current files.')) {
-                    Brink::install();
-
-                    $this->successfully();
-                } else {
-                    $this->line('That\'s okay.');
-                    $this->line('Brink will not be installed. If this was a mistake, run the command again and be sure to accept when asked for confirmation or use the --force option.');
-                }
-            } else {
-                Brink::install();
-
-                $this->successfully();
-            }
+            return $this->successfully();
         }
-    }
 
+        if ($this->confirm('A file called app.scss already exists. Are you sure you want to install Brink? This will overwrite the current files.')) {
+            Brink::install();
+            return $this->successfully();
+        } 
+
+        $this->line('That\'s okay.');
+        $this->line('Brink will not be installed. If this was a mistake, run the command again and be sure to accept when asked for confirmation or use the --force option.');
+    }
+    
     public function successfully()
     {
         $this->info('Brink installed successfully.');
